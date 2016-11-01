d9ac7def2f48c4785608dc133b50502e4bdcd99b
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-01 17:28:23 -0500

    added some new things

--- server/server.js
+++ server/server.js
@@ -56,9 +56,9 @@ function getProjects (req, res) {
 
 app.post('/yesman',function(req,res) {
     var reply = slack.respond(req.body,function(hook) {
-        let params = hook.params
+        
         return {
-            text: `${params}`,
+            text: 'hey guys',
             username: 'Bot'
         };
     });
