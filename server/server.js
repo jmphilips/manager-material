e5d72035826293dc3e53baec6ca01ee0757a9b06
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-01 15:51:30 -0500

    Changed the params

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ function getProjects (req, res) {
 app.post('/yesman',function(req,res) {
     var reply = slack.respond(req.body,function(hook) {
         return {
-            text: `${req.body.text}`,
+            text: `${hook.params}`,
             username: 'Bot'
         };
     });
