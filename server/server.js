dca59c01fd716879082a5a026fdba01c52d2a294
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-02 12:40:18 -0500

    trying to fix it

--- server/server.js
+++ server/server.js
@@ -113,29 +113,18 @@ app.post('/slack-slash/update-project', function(req, res){
       Project.findOneAndUpdate({"title": title}, {$push: {updates: {message: update, timeStamp: moment()}}}, {upsert: true}, {new: true})
       .then(project => {
 
-              transporter.sendMail({
-  from: 'project.manager.helper@gmail.com',
-  to: `${project.email}`,
-  subject: `Updates about ${project.title}`,
-  text: `${project.updates}`
-}, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(`Message sent`);
-  }
-});
+          console.log(project)
 
-     var body = {
-        response_type: "in_channel",
-        "attachments": [
-          {
-            "text": "Updated"
-         
-          }
-        ]
-      };
-            res.send(body);
+        var body = {
+            response_type: "in_channel",
+            "attachments": [
+            {
+                "text": "Updated"
+            
+            }
+            ]
+        };
+    res.send(body);
 
 
 
