dd4dda1d5adefd8efe904f6a5ebf3981cba906cd
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-03 09:14:40 -0500

    fixed a typo

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.post('/slack-slash/get-project-updates', function(req, res){
         .then(project => {
 
         let newString = "";
-        project.updates.reversegi.forEach(update => {newString += `${moment(update.timeStamp).utcOffset(-5).format('MMM DD h:mm a')}: ${update.message}\n`})
+        project.updates.reverse.forEach(update => {newString += `${moment(update.timeStamp).utcOffset(-5).format('MMM DD h:mm a')}: ${update.message}\n`})
 
             var body = {
                 response_type: "in_channel",
