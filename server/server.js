dbea387490e690c3be7221f0b922a1beae4e162a
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-03 09:11:21 -0500

    reversed the array

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.post('/slack-slash/get-project-updates', function(req, res){
         
 
         let newString = "";
-        project.reversegi.updates.forEach(update => {newString += `${moment(update.timeStamp).utcOffset(-5).format('MMM DD h:mm a')}: ${update.message}\n`})
+        project.reverse.updates.forEach(update => {newString += `${moment(update.timeStamp).utcOffset(-5).format('MMM DD h:mm a')}: ${update.message}\n`})
 
             var body = {
                 response_type: "in_channel",
